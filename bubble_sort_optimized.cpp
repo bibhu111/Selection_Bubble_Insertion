dfe10109c0fc78dd5495628866042dbcdf1fc2d5
bibhuDhar <bibhudharcs@gmail.com>
2024-11-01 08:26:56 +0600

    optimized_bubble

---
+++ bubble_sort_optimized.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+void bubble_sort(int arr[],int n){
+  for(int i=0;i<n-1;i++){
+    int didSum=0;
+    for(int j=0;j<n-i-1;j++){
+      if(arr[j]>arr[j+1]){
+        swap(arr[j],arr[j+1]);
+        didSum=1;
+      }
+      if(didSum=0) break;
+    }
+  }
+}
+int main()
+{
+  int n;
+  cin>>n;
+  int arr[n];
+  for(int i=0;i<n;i++){
+    cin>>arr[i];
+  }
+  bubble_sort(arr,n);
+  for(int i=0;i<n;i++){
+    cout<<arr[i]<<" ";
+  }
+}
